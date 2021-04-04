d7a3e286987ed78041fa6a81751a4ce758044c62
roshan <78849869+PS99003576@users.noreply.github.com>
2021-04-04 19:37:10 +0530

    Update PrimeNumbers.kt

--- Assessment/app/src/main/java/com/ltts/assessment/PrimeNumbers.kt
+++ Assessment/app/src/main/java/com/ltts/assessment/PrimeNumbers.kt
@@ -1,31 +1,29 @@
 package com.ltts.assessment
 
-open class PrimeNumbers {
-    fun checkPrimeNumber(num: Int): Boolean {
-        var flag = true
-        for (i in 2..num / 2) {
-            if (num % i == 0) {
-                flag = false
-                break
-            }
-        }
-        return flag
+class PrimeNumbers{
+   fun checkPrimeNumber(number:Int){
+       var count = 1
+       var i=2
+       var j:Int
+       var flag:Int
+       while (count <= number){
+           flag = 0
+           for (j in  2..i/2){
+               if(i%j == 0)
+               {
+                   flag = 1
+                   break
+               }
+           }
+           if(flag == 0){
+               println(i)
+               count++
+           }
+           i++
+       }
     }
-}
-
-class Check:PrimeNumbers(){
-    fun primeNumbersView(number: Int) {
-        var low = 2
-        val high = number
-        while (low < high) {
-            if (checkPrimeNumber(low))
-                print(low.toString() + " ")
-            ++low
-        }
-    }
-
 }
 
 fun main(args:Array<String>){
-      Check().primeNumbersView(Integer.valueOf(readLine()))
+    PrimeNumbers().checkPrimeNumber(Integer.valueOf(readLine()))
 }
